47f9917ad063dcbf39b3b1709724e32388aedc09
Marius RANGU <132186805+MariusRangu@users.noreply.github.com>
2023-05-11 22:58:41 +0300

    Hai cu mergiu! (#2)
    * MERGIU,   aka Squashed commit of the following:
    
    commit 739e11e55f170320551f86b46e68a87487b15543
    Author: MariusNOYB <131708034+MariusNOYB@users.noreply.github.com>
    Date:   Thu May 11 18:42:51 2023 +0300
    
        Original smartness
    
    * Squashed commit of the following:
    
    commit 1128c948830f36eeacbafcbb51af1e29a78da58a
    Author: MariusRangu <marius.rangu@rsxengineering.com>
    Date:   Thu May 11 21:02:50 2023 +0300
    
        +
    
    commit 3260166ba7e711cfbbef365753f863fc9e0df842
    Author: MariusRangu <marius.rangu@rsxengineering.com>
    Date:   Thu May 11 20:06:18 2023 +0300
    
        Revert "Merge commit 'ed906d89563c625629f988ba1dcab69f80aecc1a' into 'Blink_smarter'-feature"
    
        This reverts commit cc9febb9167b49cac0b79f76da0d45ea92a55664, reversing
        changes made to b6668733bdf8bfbf528a5097f69ba70bd4990406.
    
    commit cc9febb9167b49cac0b79f76da0d45ea92a55664
    Merge: b666873 ed906d8
    Author: MariusRangu <marius.rangu@rsxengineering.com>
    Date:   Thu May 11 20:05:28 2023 +0300
    
        Merge commit 'ed906d89563c625629f988ba1dcab69f80aecc1a' into 'Blink_smarter'-feature
    
    commit b6668733bdf8bfbf528a5097f69ba70bd4990406
    Author: MariusRangu <marius.rangu@rsxengineering.com>
    Date:   Thu May 11 19:30:16 2023 +0300
    
        Squashed commit of the following:
    
        commit 5ce134809f0b3658a44ad32f2d940d997e13609e
        Author: MariusRangu <marius.rangu@rsxengineering.com>
        Date:   Thu May 11 19:19:09 2023 +0300
    
            Squashed commit of the following:
    
            commit 739e11e55f170320551f86b46e68a87487b15543
            Author: MariusNOYB <131708034+MariusNOYB@users.noreply.github.com>
            Date:   Thu May 11 18:42:51 2023 +0300
    
                Original smartness
    
    commit c927427f89694d485cc0943207c12fcd38926ee6
    Author: MariusRangu <marius.rangu@rsxengineering.com>
    Date:   Thu May 11 18:56:12 2023 +0300
    
        Work done!
    
    commit c109862591113793ded3e1dbe0e82439bd7f2f22
    Author: MariusRangu <marius.rangu@rsxengineering.com>
    Date:   Thu May 11 18:41:31 2023 +0300
    
        Included smartness
    
    * Squashed commit of the following:
    
    commit 11535abc4ebb5d618c8774ab34fc59dae9af1457
    Author: MariusNOYB <131708034+MariusNOYB@users.noreply.github.com>
    Date:   Thu May 11 22:37:11 2023 +0300
    
        work done.

--- blink/main/blink_example_main.c
+++ blink/main/blink_example_main.c
@@ -14,6 +14,7 @@
 #include "esp_log.h"
 #include "led_strip.h"
 #include "sdkconfig.h"
+#include "blink smarter.h"      // or don't... +
 #include "me"
 #include "chatGPT"
 // Omg, I worked so much!
---
+++ blink/managed_components/espressif__led_strip/include/blink_smarter.h
@@ -0,0 +1,4 @@
+#ifndef BLINK_SMART
+#define BLINK_SMART
+
+#endif // BLINK_SMART
